7141bddf7ae8ea33a97493efbd9a84dfc88a8dfe
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-16 17:49:43 -0600

    [UPDATED] added new style

--- dashboard/verificacion.php
+++ dashboard/verificacion.php
@@ -27,7 +27,7 @@ $codigo = $_GET['codigo'];
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Asset/css/style.css">
+    <link rel="stylesheet" href="../Asset/css/styles.css">
     <title>5B | Registro Conéctate</title>
   </head>
   <body>
