e80886532fdb49e5a1e248b3cca06fed4b16586b
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-17 00:57:02 -0600

    [UPDATED] added new styles

--- dashboard/verificacion.php
+++ dashboard/verificacion.php
@@ -27,8 +27,10 @@ $codigo = $_GET['codigo'];
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../Asset/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="../Asset/css/styles.css">
-    <title>5B | Registro Conéctate</title>
+    <title>5B | Foro de Innovación</title>
   </head>
   <body>
       
@@ -164,6 +166,7 @@ $codigo = $_GET['codigo'];
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="./../Asset/js/owl.carousel.min.js"></script>
     <script src="./../Asset/js/main.js"></script>
     <script src="./../Asset/js/events.js"></script>
     <script>
